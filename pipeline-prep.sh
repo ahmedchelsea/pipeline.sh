bc96754a3f4226e4562660658146246f68bdd0ec
Chelsea Ahmed <chelseaahmed@Chelseas-MBP.attlocal.net>
2023-02-04 08:49:09 -0500

    add changes

---
+++ pipeline-prep.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# add jenkins as a user
+sudo useradd jenkins
+
+# give jenkins user a passwd
+echo 'jenkins:ahmeduser123@#' | chpasswd
+
+# patching server first
+yum install update -y
+
+yum intsall upgrade -y 
+
+# install docker
+yum install docker.io -y
+
+# add jenkins user to docker group
+usermod -aG docker jenkins 
+
+# add user jenkins to sudoers group and run all commands without asking for passwd
+echo "jenkins  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# switch to user jenkins ih the system
+su -jenkins
+
+
+
+
+
